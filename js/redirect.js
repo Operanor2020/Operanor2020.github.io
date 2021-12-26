75f9cea845e790968e8432e5dc6795127a4fffc9
Operanor2020 <62067556+Operanor2020@users.noreply.github.com>
2021-12-26 23:33:05 +0300

    Delete js directory

--- js/redirect.js
+++
@@ -1,8 +0,0 @@
-import { MongoClient } from "mongoose";
-const Client = new MongoClient('mongodb+srv://Nuggets:<password>@nuggets.irqty.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
-try{
-    await Client.connect()
-}catch(err){
-    console.log('Не удалось подключиться к базе данных: ', err)
-}
-
